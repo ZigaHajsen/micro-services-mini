85675f59c36eee5e6a8bbef17042103833ee7d4d
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-07 12:30:47 +0200

    used query service

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,23 +1,4 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-export const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-
-    // eslint-disable-next-line
-  }, []);
-
+export const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
