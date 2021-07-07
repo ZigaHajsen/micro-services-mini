474183c51d2c625e297885d9fe0e009fe2af345e
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-07 17:32:02 +0200

    added event sync

--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type == 'PostCreated') {
     const { id, title } = data;
 
@@ -45,10 +40,33 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002');
+
+  try {
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+      const { type, data } = event;
+      console.log('Processing event:', type);
+
+      handleEvent(type, data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
