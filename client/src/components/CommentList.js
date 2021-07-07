348701436fb7459392ff059a1ab21879e47c2dcd
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-07 17:06:58 +0200

    frontend moderation

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,6 +1,21 @@
 export const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    const { status, content, id } = comment;
+    let shownContent;
+
+    if (status === 'approved') {
+      shownContent = content;
+    }
+
+    if (status === 'pending') {
+      shownContent = 'This comment is awaiting moderation';
+    }
+
+    if (status === 'rejected') {
+      shownContent = 'This comment has been rejected';
+    }
+
+    return <li key={id}>{shownContent}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
